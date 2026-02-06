fe39a9276aa65fef19b22b16cb33fd613b7e5fd1
Kalpesh Mevada <kp.codeandlife@gmail.com>
2026-02-06 15:18:52 +0530

    modify files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import Services from './Pages/Services';
-import ContactUs from './Pages/ContactUs';
-import AboutUs from './Pages/AboutUs';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import ContactUs from "./Pages/ContactUs";
+import AboutUs from "./Pages/AboutUs";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' Component={Home} />
-        <Route path='/' Component={Services} />
-        <Route path='/' Component={ContactUs} />
-        <Route path='/' Component={AboutUs} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/" Component={Services} />
+          <Route path="/" Component={ContactUs} />
+          <Route path="/" Component={AboutUs} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
-};
+}
 
 export default App;
